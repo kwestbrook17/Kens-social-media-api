c135ea9eae435aff63db07dec16d7bc6cbce3b16
Kenneth Westbrook <kennethwestbrook17@gmail.com>
2023-11-25 16:57:13 -0800

    initial commit

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    first: String,
+    last: String,
+    age: Number,
+  },
+  {
+    toJSON: {
+      // TODO: Mongoose will not include virtuals by default, so add a `virtuals` property and set it's value to true
+    },
+    id: false,
+  }
+);
+
+// TODO: Create a virtual property `fullName` on the userSchema
+userSchema.virtual('fullName').get(function() {
+  return `${this.first} ${this.last}`;
+});
+
+
+// TODO: Create a getter for the virtual that returns the full name of the user (first + last)
+userSchema.virtual('fullName').set(function(fullName) {
+  const [first, last] = fullName.split(' ');
+  this.set({ first, last });
+});
+
+
+// TODO: Create a setter for the virtual that sets the value of the first and last name, given just the `fullName`
+userSchema.virtual('fullName').set(function(fullName) {
+  const [first, last] = fullName.split(' ');
+  this.set({ first, last });
+});
+
+
+// Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
